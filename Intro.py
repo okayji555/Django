08f0e35babf4df62db77176e9ffaf38e1c6cf314
Omkar Gore <yothisisokg@gmail.com>
2026-05-13 10:32:16 +0530

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -17,5 +17,14 @@ step4: after download, start a new project
       code: django-admin startproject {project name}
       eg: django-admin startproject hello
 
+step5: the project will start, now open it in url
+      code: python manage.py runserver
+#this will run the server, to continue in the terminal, press ctrl+C
+
+step6: now create app
+      code: python manage.py startup {app name}
+      eg: python manage.py startup cuties
+
+START YOUR CODING:
 
 """      
