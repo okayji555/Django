55f2d4976a239005aa8fbfde3033bf85f79ee156
Omkar Gore <yothisisokg@gmail.com>
2026-05-13 08:36:18 +0530

    Create Intro.py

---
+++ Intro.py
@@ -0,0 +1,21 @@
+"""
+Django is a Python framework that makes it easier to create web sites using Python.
+
+step1 : CREATE a virtual environment
+      code: python -m venv {name}
+      eg: python -m venv what
+
+step2: activate the virtual environment
+      code: {name}\Scripts\activate
+      eg: what\Scripts\activate
+
+step3: download Django
+      code: python -m pip install Django
+      check version: django-admin --version
+
+step4: after download, start a new project
+      code: django-admin startproject {project name}
+      eg: django-admin startproject hello
+
+
+"""      
